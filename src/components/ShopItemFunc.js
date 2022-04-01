bec2de34d07e3d9457bf46269829760246ae12d5
Aleksandr Shatskikh <work@errand.ru>
2022-04-01 12:53:45 +0300

    feat: it's working

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import ItemModel from "../models/ItemMode";
+
+function ShopItemFunc(props) {
+  const {item} = props;
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">
+        {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+
+  )
+}
+
+ItemModel.propTypes = {
+  item: PropTypes.instanceOf(ItemModel).isRequired
+}
+
+export default ShopItemFunc;
